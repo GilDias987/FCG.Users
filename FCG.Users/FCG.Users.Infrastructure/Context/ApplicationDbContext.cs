c91409c4574468b54768d40ee6c248468650c3c8
Miguel084 <134954882+Miguel084@users.noreply.github.com>
2026-03-12 00:12:23 -0300

    Refactor ApplicationDbContext constructor and properties

--- FCG.Users/FCG.Users.Infrastructure/Context/ApplicationDbContext.cs
+++ FCG.Users/FCG.Users.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,49 +1,26 @@
-﻿using FCG.Users.Domain.Entities;
+using FCG.Users.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FCG.Users.Infrastructure.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly string _connectionString;
-
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            _connectionString = configuration.GetConnectionString("ConnectionStrings");
-        }
-
-        public ApplicationDbContext(string connectionString)
-        {
-            _connectionString = connectionString;
         }
 
         #region DbSet
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserGroup> UsersGroup { get; set; }
-
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<UserGroup> UsersGroup { get; set; } = default!;
         #endregion
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_connectionString, builder => builder.EnableRetryOnFailure());
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
+``
