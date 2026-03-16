418f74ea4d98f1d42118b608cc1e450dd5931d71
gilfi <gilfi@GIL>
2026-03-16 09:30:20 -0300

    Ajustes para envio azure function

---
+++ FCG.Users/FCG.Users.Application.Interface/Service/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using FCG.Users.Application.Dto.Email;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Users.Application.Interface.Service
+{
+    public interface IEmailService
+    {
+        public EmailMessageDto EmailMessage(string email, string name);
+    }
+}
