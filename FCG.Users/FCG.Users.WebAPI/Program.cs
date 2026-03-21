330e1347ddf897646ad295d22cf3f3a02daf3e2c
gilfi <gilfi@GIL>
2026-03-21 19:37:49 -0300

    Ajustar Logs

--- FCG.Users/FCG.Users.WebAPI/Program.cs
+++ FCG.Users/FCG.Users.WebAPI/Program.cs
@@ -15,7 +15,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configura o ILogger para ler o appsettings.json
-//builder.AddLogging();
+builder.AddLogging();
 
 // Add services to the container.
 
