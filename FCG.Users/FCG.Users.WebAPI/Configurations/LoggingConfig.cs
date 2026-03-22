10236b0310979d33d6139ed0e435881248746781
gilfi <gilfi@GIL>
2026-03-22 13:34:04 -0300

    Adicionar Logs

--- FCG.Users/FCG.Users.WebAPI/Configurations/LoggingConfig.cs
+++ FCG.Users/FCG.Users.WebAPI/Configurations/LoggingConfig.cs
@@ -1,27 +1,19 @@
-﻿namespace FCG.Users.WebAPI.Configurations
+﻿using Serilog;
+
+namespace FCG.Users.WebAPI.Configurations
 {
     public static class LoggingConfig
     {
         public static void AddLogging(this WebApplicationBuilder builder)
         {
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
+            Log.Logger = new LoggerConfiguration()
+                     .ReadFrom.Configuration(builder.Configuration)
+                     .Enrich.FromLogContext()
+                     .WriteTo.Console()
+                     .WriteTo.Debug()
+                     .CreateLogger();
 
-            // 1. Adiciona o SDK do Application Insights (Telemetria de requisições, etc.)
-            builder.Services.AddApplicationInsightsTelemetry();
-
-            // 2. Configura especificamente o Logging para o Application Insights
-            builder.Logging.AddApplicationInsights(
-                //configureTelemetryConfiguration: (config) =>
-                //config.ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"],
-                configureApplicationInsightsLoggerOptions: (options) => 
-                {
-                    options.IncludeScopes = true;
-                    options.TrackExceptionsAsExceptionTelemetry = false;
-                }
-            );
-
-            builder.Logging.SetMinimumLevel(LogLevel.Information); // Set a minimum log level
+            builder.Host.UseSerilog();
         }
     }
 }
